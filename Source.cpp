c94dc943b407ce7824957ca9586045c73b574891
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-02-01 17:51:41 +0100

    Added class Point and SumElements method

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include<ctime>
+
+class Point
+{
+public:
+	Point() :x(0), y(0) {};
+	Point(int pX, int pY) :x(pX), y(pY) {};
+	int getX() { return x; }
+	int getY() { return y; }
+	void setX(int pX) { x = pX; }
+	void setY(int pY) { y = pY; }
+	~Point(){}
+
+private:
+	int x;
+	int y;
+};
+
 template<typename T>
 class DynArray
 {
@@ -22,12 +40,14 @@ public:
 			<< size << " elements, for " << this
 			<< '\n';
 	}
-	int getElem(size_t idx) { return arr[idx]; }
+	int  getElem(size_t idx) { return arr[idx]; }
 	void setElem(size_t idx, T val) { arr[idx] = val; }
 	void print();
 	void randomize();
 	void Add(T);
-	void Delete(int elemIndex);
+	void Delete(size_t elemIndex);
+	T sumElements();
+	friend std::ostream& operator <<(std::ostream& os, Point& obj);
 	~DynArray()
 	{
 		delete[] arr;
@@ -47,11 +67,12 @@ void DynArray<T>::print()
 template<typename T>
 void DynArray<T>::randomize()
 {
-		for (size_t i{ 0 }; i < size; ++i)
+		for (size_t i = 0; i < size; i++)
 		{
-			arr[i] = rand() % 10;
+			arr[i] = (T)(480 + rand() % 90) * 0.1;
 		}
 }
+
 
 template<typename T>
  void DynArray<T>::Add(T newElem)
@@ -77,10 +98,10 @@ template<typename T>
 }
 
  template<typename T>
- void DynArray<T>::Delete(int elemIndex)
+ void DynArray<T>::Delete(size_t elemIndex)
  {
 	 DynArray tmp(size - 1);
-	 int j = 0;
+	 size_t j = 0;
 	 for (size_t i = 0; i < tmp.size; i++)
 	 {
 		 if (i == elemIndex - 1)
@@ -104,19 +125,78 @@ template<typename T>
 	 }
  }
 
+ template<typename T>
+ T DynArray<T>::sumElements()
+ {
+	 T tmp = 0;
+	 for (size_t i = 0; i < size; i++)
+	 {
+		 tmp += arr[i];
+	 }
+	 return tmp;
+ }
+ template<>
+ Point DynArray<Point>::sumElements()
+ {
+	 int sumX = 0;
+	 int sumY = 0;
+	 for (size_t i = 0; i < size; i++)
+	 {
+		 sumX += arr[i].getX();
+		 sumY += arr[i].getY();
+	 }
+	 std::cout << "The sum of X coordinates: " << sumX << std::endl;
+	 std::cout << "The sum of Y coordinates: " << sumY << std::endl;
+	 return arr[size];
+ }
 
+ template<>
+ void DynArray<Point>::randomize()
+ {
+	 for (size_t i = 0; i < size; i++)
+	 {
+		 arr[i].setX((int)(480 + rand() % 90) /10);
+		 arr[i].setY((int)(480 + rand() % 90) /10);
+	 }
+ }
+ std::ostream& operator <<(std::ostream& os,  Point& obj)
+ {
+	 os << "X: " << obj.getX() << std::endl;
+	 os << "Y: " << obj.getY() << std::endl;
+	 return os;
+ }
 int main()
 {
-	DynArray<int> ar1{ 10 };
-	ar1.randomize();
-	std::cout << "ar1 elements: ";
-	ar1.print();
-	DynArray<int> ar2{ ar1 };
-	std::cout << "ar2 elements: ";
-	ar2.print();
-	ar2.Add(0);
-	ar2.print();
-	ar2.Delete(3);
-	ar2.print();
+	srand(static_cast<int>(time(NULL)));
+	std::cout << "Array of int's" << std::endl;
+	DynArray<int> arr(2);
+	arr.randomize();
+	arr.print();
+	std::cout << arr.sumElements() << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Array of doubles" << std::endl;
+	DynArray<float> arr1(2);
+	arr1.randomize();
+	arr1.print();
+	std::cout << arr1.sumElements() << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Array of char's" << std::endl;
+	DynArray<char> arr2(2);
+	arr2.randomize();
+	arr2.print();
+	std::cout << arr2.sumElements() << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Array of Points" << std::endl;
+	DynArray<Point> arr3(2);
+	arr3.randomize();
+	arr3.print();
+	arr3.sumElements();
+
 	return 0;
 }
